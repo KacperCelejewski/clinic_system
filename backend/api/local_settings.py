25683b10e8eda9e918234c0b2e31074a6ba17540
Kacper Celejewski <kacpercele@gmail.com>
2023-12-03 18:33:45 +0100

    fix/env variables added to docker compose and db url

--- backend/api/local_settings.py
+++
@@ -1,7 +0,0 @@
-postgrsql = {
-    "pguser": "postgres",
-    "pgpassword": "kacperek20066",
-    "pghost": "postgres",
-    "pgport": 5432,
-    "pgdb": "user",
-}
--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import api.local_settings as local_settings
+import os
 
 app = Flask(__name__)
 CORS(app)
-
-pg = local_settings.postgrsql
+pg = {
+    "pguser": os.environ["PG_USER"],
+    "pgpassword": os.environ["PG_PASSWORD"],
+    "pghost": os.environ["PG_HOST"],
+    "pgport": os.environ["PG_PORT"],
+    "pgdb": os.environ["PG_DB"],
+}
 db_uri = f"postgresql://{pg['pguser']}:{pg['pgpassword']}@{pg['pghost']}:{pg['pgport']}/{pg['pgdb']}"
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 db = SQLAlchemy(app)
